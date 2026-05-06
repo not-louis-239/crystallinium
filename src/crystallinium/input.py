8903d4353a4fa96995814f9c3280d2d4c91147bd
not-louis-239 <louis.boulton@icloud.com>
2026-05-06 17:54:53 +0800

    Add input module

---
+++ src/crystallinium/input.py
@@ -0,0 +1,55 @@
+from enum import IntEnum
+
+import pygame as pg
+from pygame.key import ScancodeWrapper
+
+class MouseButton(IntEnum):
+    LMB = 0
+    MMB = 1
+    RMB = 2
+
+class InputManager:
+    def __init__(self) -> None:
+        self.dt_s: float = 0
+        self.events: list[pg.event.Event] = pg.event.get()
+        self.prev_keys: ScancodeWrapper = pg.key.get_pressed()
+        self.cur_keys: ScancodeWrapper = pg.key.get_pressed()
+
+    def update(self, new_keys: ScancodeWrapper, events: list[pg.event.Event], dt_s: float) -> None:
+        """Run this once at the start of each event loop
+
+        Usage:
+
+        >>> def update(dt_s):
+        ...     keys = pygame.key.get_pressed()
+        ...     events = pygame.event.get()
+        ...     input_manager.update(keys, events, dt_s)
+        """
+
+        self.dt_s = dt_s
+        self.events = events
+        self.prev_keys, self.cur_keys = self.cur_keys, new_keys
+
+    def is_down(self, key: int) -> bool:
+        """Returns True as long as the key is held"""
+        return self.cur_keys[key]
+
+    def went_down(self, key: int) -> bool:
+        """Returns True on the first frame the key is held down"""
+        return self.cur_keys[key] and not self.prev_keys[key]
+
+    def went_up(self, key: int) -> bool:
+        """Returns True on the first frame the key is released"""
+        return not self.cur_keys[key] and self.prev_keys[key]
+
+    def mouse_is_down(self, button: int) -> bool:
+        """Returns True as long as the mouse button is held"""
+        return pg.mouse.get_pressed()[button]
+
+    def mouse_went_down(self, button: int) -> bool:
+        """Returns True on the first frame the mouse button is held down"""
+        return any(event.type == pg.MOUSEBUTTONDOWN and event.button == button for event in self.events)
+
+    def mouse_went_up(self, button: int) -> bool:
+        """Returns True on the first frame the mouse button is released"""
+        return any(event.type == pg.MOUSEBUTTONUP and event.button == button for event in self.events)
